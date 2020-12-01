3908e941bc69014922252e1e356880494ba1a586
TheMalachite <eliasgheeraert@gmail.com>
2020-12-01 18:48:42 +0100

    [BACKPORT] treewide: Backport MTK task turbo feature from bomb-q-oss

--- kernel/fork.c
+++ kernel/fork.c
@@ -105,6 +105,9 @@
 #include <trace/events/task.h>
 
 #include <mt-plat/mtk_pidmap.h>
+#ifdef CONFIG_MTK_TASK_TURBO
+#include <mt-plat/turbo_common.h>
+#endif
 
 /*
  * Minimum number of threads to boot the kernel
@@ -1792,7 +1795,9 @@ static __latent_entropy struct task_struct *copy_process(
 	p->sequential_io	= 0;
 	p->sequential_io_avg	= 0;
 #endif
-
+#ifdef CONFIG_MTK_TASK_TURBO
+	init_turbo_attr(p, current);
+#endif
 	/* Perform scheduler related setup. Assign this task to a CPU. */
 	retval = sched_fork(clone_flags, p);
 	if (retval)
